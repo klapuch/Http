77adb64112aba0fcee163a8c50d9bd71ad4ecd57
Dominik Klapuch <klapuchdominik@gmail.com>
2016-09-19 21:54:49 +0200

    Added fake request

---
+++ Core/FakeRequest.php
@@ -0,0 +1,18 @@
+<?php
+declare(strict_types = 1);
+namespace Klapuch\Http;
+
+/**
+ * Fake
+ */
+final class FakeRequest implements Request {
+    private $response;
+
+    public function __construct(Response $response = null) {
+        $this->response = $response;
+    }
+
+    public function send(): Response {
+        return $this->response;
+    }
+}
